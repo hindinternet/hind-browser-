7fb31d8e7a91c4ba2e67256dc416df359492e4ec
hindinternet <99014678+hindinternet@users.noreply.github.com>
2025-11-19 16:00:05 +0530

    initialize core-engine
     Added engine.js with placeholder function for future business logic.

---
+++ core-engine/Engine.js
@@ -0,0 +1,11 @@
+// Core Engine placeholder
+
+// Example function
+function helloEngine() {
+    console.log("Core Engine is working!");
+}
+
+// Export functions
+module.exports = {
+    helloEngine
+};
